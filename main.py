c08e7da6390a0664f4cf23061635b3df25e918c6
Asadullah-Dal17 <aiphile17@gmail.com>
2021-03-29 23:28:49 +0500

    updating qr code detector funciton

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pyzbar.pyzbar as pyzbar
 
 # Variable
 
-camID = 0  # camera ID, or pass string as filename. to the camID
+camID = 1  # camera ID, or pass string as filename. to the camID
 
 # Real world measured Distance and width of QR code
 KNOWN_DISTANCE = 24.0  # inches
@@ -22,7 +22,7 @@ GREEN = (0, 255, 0)
 CYAN = (255, 255, 0)
 GOLD = (0, 255, 215)
 YELLOW = (0, 255, 255)
-ORANGE = (0, 165, 255)
+ORANGE = (0, 165, 230)
 
 # function
 
@@ -37,6 +37,7 @@ def eucaldainDistance(x, y, x1, y1):
 
 
 def DetectQRcode(image):
+    codeWidth = 0
 
     # convert the color image to gray scale image
     Gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
@@ -54,7 +55,15 @@ def DetectQRcode(image):
             hull = points
         n = len(hull)
         for j in range(0, n):
+            print(j, "      ", (j + 1) % n, "    ", n)
+
             cv.line(image, hull[j], hull[(j + 1) % n], ORANGE, 2)
+
+        x, x1 = hull[0][0], hull[1][0]
+        y, y1 = hull[0][1], hull[1][1]
+        euclaDistance = eucaldainDistance(x, y, x1, y1)
+
+        return euclaDistance
 
 
 # creating camera object
@@ -66,7 +75,9 @@ camera = cv.VideoCapture(camID)
 
 while True:
     ret, frame = camera.read()
-    DetectQRcode(frame)
+    codeWidth = DetectQRcode(frame)
+    print(codeWidth)
+
     cv.imshow("frame", frame)
 
     key = cv.waitKey(1)
