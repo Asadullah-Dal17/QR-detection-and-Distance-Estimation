0adde8f7a1d901c8fce79627453369d31e95eb85
Asadullah-Dal17 <aiphile17@gmail.com>
2021-03-29 16:57:46 +0500

    create #eucaldianDistance, adding some variables colors etc

--- main.py
+++ main.py
@@ -1,8 +1,41 @@
 import cv2 as cv
 import numpy as np
+import math
 
-camID = 1
+# Variable
 
+camID = 1  # camera ID, or pass string as filename. to the camID
+
+# Real world measured Distance and width of QR code
+KNOWN_DISTANCE = 24.0  # inches
+KNOWN_WIDTH = 5.0  # inches
+
+# define the fonts
+fonts = cv.FONT_HERSHEY_COMPLEX
+
+# colors (BGR)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+MAGENTA = (255, 0, 255)
+GREEN = (0, 255, 0)
+CYAN = (255, 255, 0)
+GOLD = (0, 255, 215)
+YELLOW = (0, 255, 255)
+ORANGE = (0, 165, 255)
+
+# function
+
+# finding Distance between two points
+
+
+def eucaldainDistance(x, y, x1, y1):
+
+    eucaldainDist = math.sqrt((x1 - x) ** 2 + (y1 - y) ** 2)
+
+    return eucaldainDist
+
+
+# creating camera object
 camera = cv.VideoCapture(camID)
 # TODO create QR code detector fucntion
 # TODO create Eucaldian Distance finder funciton
